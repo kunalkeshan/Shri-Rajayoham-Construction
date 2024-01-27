6d560f8a7c4163ccdddb68809a52704ba1e80047
Kunal Keshan <kunalkeshan12@gmail.com>
2024-01-27 18:45:25 +0530

    chore: adjusted social share btns with icons
    - added copy link button

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,24 +1,26 @@
-import { TooltipProvider } from "@/components/ui/tooltip";
-import Footer from "@/layouts/Footer";
-import Navbar from "@/layouts/Navbar";
-import type { Metadata } from "next";
+import type { Metadata } from 'next';
+import Footer from '@/layouts/Footer';
+import Navbar from '@/layouts/Navbar';
+import { TooltipProvider } from '@/components/ui/tooltip';
+import { Toaster } from '@/components/ui/sonner';
 
 export const metadata: Metadata = {
-    title: "Shri Rajayoham Construction Company",
-    description:
-        "Very Trustable.High Values.Complete Transparency. High Quality. Reasonable price",
+	title: 'Shri Rajayoham Construction Company',
+	description:
+		'Very Trustable.High Values.Complete Transparency. High Quality. Reasonable price',
 };
 
 export default function RootLayout({
-    children,
+	children,
 }: {
-    children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-    return (
-        <TooltipProvider>
-            <Navbar />
-            {children}
-            <Footer/>
-        </TooltipProvider>
-    );
+	return (
+		<TooltipProvider>
+			<Navbar />
+			{children}
+			<Footer />
+			<Toaster />
+		</TooltipProvider>
+	);
 }
--- components/blogs/SocialShare.tsx
+++ components/blogs/SocialShare.tsx
@@ -1,19 +1,31 @@
+'use client';
+
 import React from 'react';
-import { Button } from '@/components/ui/button';
-import { SRCC_WEBSITE_URL } from '@/constants/srcc';
 import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+import { toast } from "sonner"
+import { SRCC_WEBSITE_URL } from '@/constants/srcc';
+import { Copy } from 'lucide-react';
+import TwitterX from '../icons/TwitterX';
+import WhatsApp from '../icons/WhatsApp';
 
 type SocialShareProps = React.ComponentProps<'section'> & {
 	post: SRCC_BlogPost;
 };
 
 const SocialShare: React.FC<SocialShareProps> = ({ post }) => {
+	const LINK = `${SRCC_WEBSITE_URL}/blogs/${post.slug}`;
 	const SHARE_TEXT = encodeURI(post.title);
-	const SHARE_LINK = encodeURI(`${SRCC_WEBSITE_URL}/blogs/${post.slug}`);
+	const SHARE_LINK = encodeURI(LINK);
 
-	const WHATSAPP_TEXT = encodeURI(
-		`${SHARE_TEXT}\n\nRead here: ${SHARE_LINK}`
-	);
+	const WHATSAPP_TEXT = encodeURI(`${post.title}\n\nRead here: ${LINK}`);
+    // const LINKEDIN_TEXT = encodeURI(`mini=true&url=${LINK}&title=${post.title}&summary=${SRCC_WEBSITE_URL}&source=Shri Rajayoham Construction Company`)
+
+	const handleCopyLinkToClipboard = () => {
+		navigator.clipboard.writeText(LINK);
+        toast('Link Copied!')
+	};
+
 	return (
 		<section className='mt-8 flex items-center gap-2 flex-wrap'>
 			<Button
@@ -24,7 +36,7 @@ const SocialShare: React.FC<SocialShareProps> = ({ post }) => {
 					href={`https://wa.me/?text=${WHATSAPP_TEXT}`}
 					target='_blank'
 				>
-					WhatsApp
+					<WhatsApp className='mr-2' size={20} /> WhatsApp
 				</Link>
 			</Button>
 			<Button className='' asChild>
@@ -32,8 +44,19 @@ const SocialShare: React.FC<SocialShareProps> = ({ post }) => {
 					href={`https://twitter.com/intent/tweet?url=${SHARE_LINK}&text=${SHARE_TEXT}`}
 					target='_blank'
 				>
-					Twitter
+					<TwitterX className='mr-2' fill='#ffffff' size={20} /> Twitter
 				</Link>
+			</Button>
+			{/* <Button className='bg-blue-500 hover:bg-blue-500/80 text-white' asChild>
+				<Link
+					href={`https://www.linkedin.com/shareArticle?${LINKEDIN_TEXT}`}
+					target='_blank'
+				>
+					LinkedIn
+				</Link>
+			</Button> */}
+			<Button className='' onClick={handleCopyLinkToClipboard}>
+				<Copy className='mr-2' strokeWidth={1.5} size={20} /> Copy Link
 			</Button>
 		</section>
 	);
