05c57df88af0897b6a10f7615abeaafafed18f87
kunalkeshan <kunalkeshan12@gmail.com>
2024-01-31 17:33:26 +0530

    chore: using intl num formatter on cost fields

--- components/packages/QuotationCalculator.tsx
+++ components/packages/QuotationCalculator.tsx
@@ -1,5 +1,6 @@
-"use client";
-import React, { useEffect, useState } from "react";
+'use client';
+
+import React, { useEffect, useState } from 'react';
 import {
 	Table,
 	TableBody,
@@ -8,8 +9,22 @@ import {
 	TableHead,
 	TableHeader,
 	TableRow,
-} from "@/components/ui/table";
-import { Input } from "@/components/ui/input";
+} from '@/components/ui/table';
+import { Input } from '@/components/ui/input';
+
+const costFormatter = (num: number) =>
+	new Intl.NumberFormat('en-IN', {
+		notation: 'standard',
+		style: 'currency',
+		currency: 'INR',
+	}).format(num);
+
+const RATES = {
+	area: 2250,
+	waterSump: 18,
+	septicTank: 18,
+	compoundWall: 425,
+};
 
 const QuotationCalculator = () => {
 	const [total, setTotal] = useState(0);
@@ -32,22 +47,26 @@ const QuotationCalculator = () => {
 
 	const calculateCompoundWall = (): number => {
 		if (Number(inputs.length) && Number(inputs.height)) {
-			return Number(inputs.length) * Number(inputs.height) * 425;
+			return (
+				Number(inputs.length) *
+				Number(inputs.height) *
+				RATES.compoundWall
+			);
 		} else {
 			return 0;
 		}
 	};
 
 	const calculateArea = (): number => {
-		return Number(inputs.area) * 2250;
+		return Number(inputs.area) * RATES.area;
 	};
 
 	const calculateWaterSump = (): number => {
-		return Number(inputs.waterSump) * 18;
+		return Number(inputs.waterSump) * RATES.waterSump;
 	};
 
 	const calculateSepticTank = (): number => {
-		return Number(inputs.septicTank) * 18;
+		return Number(inputs.septicTank) * RATES.septicTank;
 	};
 
 	const calculateTotal = (): number => {
@@ -65,122 +84,125 @@ const QuotationCalculator = () => {
 		calculateSepticTank();
 		calculateWaterSump();
 		setTotal(calculateTotal());
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [inputs]);
 
 	return (
-		<Table className="rounded-lg">
-			<TableCaption className="pb-4">
-				Total Construction Cost{" "}
-				<span className="font-bold text-xl">₹ {total}</span>
+		<Table className='rounded-lg'>
+			<TableCaption className='pb-4'>
+				Total Construction Cost{' '}
+				<span className='font-bold text-xl'>
+					{costFormatter(total)}
+				</span>
 			</TableCaption>
 			<TableHeader>
 				<TableRow>
-					<TableHead className=" min-w-[100px] text-center">
+					<TableHead className=' min-w-[100px] text-center'>
 						Sl. No
 					</TableHead>
-					<TableHead className="min-w-[400px] text-center">
+					<TableHead className='min-w-[400px] text-center'>
 						Work
 					</TableHead>
-					<TableHead className="min-w-[300px] text-center">
+					<TableHead className='min-w-[300px] text-center'>
 						Area
 					</TableHead>
-					<TableHead className="text-center">Unit</TableHead>
-					<TableHead className="text-center min-w-[100px]">
+					<TableHead className='text-center'>Unit</TableHead>
+					<TableHead className='text-center min-w-[100px]'>
 						Rate
 					</TableHead>
-					<TableHead className="text-center">Cost</TableHead>
+					<TableHead className='text-center'>Cost</TableHead>
 				</TableRow>
 			</TableHeader>
 			<TableBody>
 				<TableRow>
-					<TableCell className="font-medium text-center">1</TableCell>
-					<TableCell className="text-center">
+					<TableCell className='font-medium text-center'>1</TableCell>
+					<TableCell className='text-center'>
 						Enter The Total Required Built Up Area All Floors
-						Including Headroom{" "}
+						Including Headroom{' '}
 					</TableCell>
 					<TableCell>
 						<Input
-							type="number"
+							type='number'
 							min={0}
-							onChange={handleChange("area")}
-							placeholder="Area in Sq.Ft"
+							onChange={handleChange('area')}
+							placeholder='Area in Sq.Ft'
 						/>
 					</TableCell>
-					<TableCell className="text-center">Sq.Ft</TableCell>
-					<TableCell className="text-center">
-						<div>₹ 2250</div>
+					<TableCell className='text-center'>Sq.Ft</TableCell>
+					<TableCell className='text-center'>
+						<div>₹ {RATES.area}</div>
 					</TableCell>
-					<TableCell className="text-center">
-						<div>₹ {calculateArea()}</div>
+					<TableCell className='text-center'>
+						<div>{costFormatter(calculateArea())}</div>
 					</TableCell>
 				</TableRow>
 				<TableRow>
-					<TableCell className="font-medium text-center">2</TableCell>
-					<TableCell className="text-center">
+					<TableCell className='font-medium text-center'>2</TableCell>
+					<TableCell className='text-center'>
 						Size Of RCC Water Sump
 					</TableCell>
 					<TableCell>
 						<Input
-							type="number"
+							type='number'
 							min={0}
-							onChange={handleChange("waterSump")}
-							placeholder="No. of Liters"
+							onChange={handleChange('waterSump')}
+							placeholder='No. of Liters'
 						/>
 					</TableCell>
-					<TableCell className="text-center">Liters</TableCell>
-					<TableCell className="text-center">
-						<div>₹ 18</div>
+					<TableCell className='text-center'>Liters</TableCell>
+					<TableCell className='text-center'>
+						<div>₹ {RATES.waterSump}</div>
 					</TableCell>
-					<TableCell className="text-center">
-						<div>₹ {calculateWaterSump()} </div>
+					<TableCell className='text-center'>
+						<div>{costFormatter(calculateWaterSump())} </div>
 					</TableCell>
 				</TableRow>
 				<TableRow>
-					<TableCell className="font-medium text-center">3</TableCell>
-					<TableCell className="text-center">
+					<TableCell className='font-medium text-center'>3</TableCell>
+					<TableCell className='text-center'>
 						Size Of Septic Tank
 					</TableCell>
 					<TableCell>
 						<Input
-							type="number"
+							type='number'
 							min={0}
-							onChange={handleChange("septicTank")}
-							placeholder="No. of Liters"
+							onChange={handleChange('septicTank')}
+							placeholder='No. of Liters'
 						/>
 					</TableCell>
-					<TableCell className="text-center">Sq.Ft</TableCell>
-					<TableCell className="text-center">
-						<div>₹ 18</div>
+					<TableCell className='text-center'>Sq.Ft</TableCell>
+					<TableCell className='text-center'>
+						<div>₹ {RATES.septicTank}</div>
 					</TableCell>
-					<TableCell className="text-center">
-						<div>₹ {calculateSepticTank()} </div>
+					<TableCell className='text-center'>
+						<div>{costFormatter(calculateSepticTank())} </div>
 					</TableCell>
 				</TableRow>
 				<TableRow>
-					<TableCell className="font-medium text-center">4</TableCell>
-					<TableCell className="text-center">
+					<TableCell className='font-medium text-center'>4</TableCell>
+					<TableCell className='text-center'>
 						Plain Compound Wall
 					</TableCell>
-					<TableCell className="flex gap-4">
+					<TableCell className='flex gap-4'>
 						<Input
-							type="number"
+							type='number'
 							min={0}
-							onChange={handleChange("length")}
-							placeholder="Length"
+							onChange={handleChange('length')}
+							placeholder='Length'
 						/>
 						<Input
-							type="number"
+							type='number'
 							min={0}
-							onChange={handleChange("height")}
-							placeholder="Height"
+							onChange={handleChange('height')}
+							placeholder='Height'
 						/>
 					</TableCell>
-					<TableCell className="text-center">Sq.Ft</TableCell>
-					<TableCell className="text-center">
-						<div>₹ 425</div>
+					<TableCell className='text-center'>Sq.Ft</TableCell>
+					<TableCell className='text-center'>
+						<div>₹ {RATES.compoundWall}</div>
 					</TableCell>
-					<TableCell className="text-center">
-						<div>₹ {calculateCompoundWall()}</div>
+					<TableCell className='text-center'>
+						<div>{costFormatter(calculateCompoundWall())}</div>
 					</TableCell>
 				</TableRow>
 			</TableBody>
