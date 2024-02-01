b00c0686593ba153bff37a9b3971842f801af5b9
surendar pd <surendarpd007@gmail.com>
2024-02-01 20:47:09 +0530

    Update global scroll behavior and add icons to project hero section

--- components/projects/Hero.tsx
+++ components/projects/Hero.tsx
@@ -1,23 +1,66 @@
+import { MoveDownIcon, RedoIcon } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Hero = () => {
 	return (
-		<section className="p-4 md:px-16 lg:max-w-7xl lg:mx-auto pb-8 md:pb-16 lg:pb-32 w-full h-[500px]">
-			<div className="w-full h-full bg-slate-200 grid grid-cols-1 gap-4">
-				<div className="col-span-1 w-full h-full bg-red-50">
-					<Image src={""} alt="" width={100} height={100} />
+		<section className="p-4 md:px-16 lg:max-w-7xl lg:mx-auto py-8 md:py-16 lg:pb-32 w-full h-full">
+			<div className="w-full grid grid-cols-1 lg:grid-cols-3 gap-4 lg:gap-16">
+				<div className="hidden lg:flex col-span-1 w-full h-full items-center justify-center">
+					<MoveDownIcon size={96} className="lg:hidden" />
+					<RedoIcon
+						size={96}
+						className="hidden lg:block -rotate-[36deg]"
+					/>
 				</div>
-				<div className="col-span-1 w-full h-full bg-red-50">arrow</div>
-				<div className="col-span-1 w-full h-full bg-red-50"></div>
-				<div className="col-span-1 w-full h-full bg-red-50">arrow</div>
-				<div className="col-span-1 w-full h-full bg-red-50"></div>
-				<div className="col-span-1 w-full h-full bg-red-50">arrow</div>
+				<Link href={"#upcoming"} className="col-span-1 w-full group overflow-hidden rounded-lg">
+					<Image
+						src={"/assets/fallback/icon-grid.svg"}
+						alt=""
+						className="w-full rounded-lg group-hover:scale-105 transition-all duration-300"
+						width={100}
+						height={100}
+					/>
+					<h2 className="text-center text-lg md:text-xl lg:text-2xl font-semibold mt-4">
+						Upcoming
+					</h2>
+				</Link>
+				<div className="col-span-1 w-full h-full flex items-center justify-center">
+					<MoveDownIcon size={96} className="lg:hidden" />
+					<RedoIcon
+						size={96}
+						className="hidden lg:block rotate-[54deg]"
+					/>
+				</div>
+				<Link href={"#ongoing"} className="col-span-1 w-full group overflow-hidden rounded-lg">
+					<Image
+						src={"/assets/fallback/icon-grid.svg"}
+						alt=""
+						className="w-full rounded-lg group-hover:scale-105 transition-all duration-300"
+						width={100}
+						height={100}
+					/>
+					<h2 className="text-center text-lg md:text-xl lg:text-2xl font-semibold mt-4">
+						Ongoing
+					</h2>
+				</Link>
+				<div className="col-span-1 w-full h-full flex items-center justify-center">
+					<MoveDownIcon size={96} className="lg:rotate-90" />
+				</div>
+				<Link href={"#completed"} className="col-span-1 w-full group overflow-hidden rounded-lg">
+					<Image
+						src={"/assets/fallback/icon-grid.svg"}
+						alt=""
+						className="w-full rounded-lg group-hover:scale-105 transition-all duration-300"
+						width={100}
+						height={100}
+					/>
+					<h2 className="text-center text-lg md:text-xl lg:text-2xl font-semibold mt-4">
+						Completed
+					</h2>
+				</Link>
 			</div>
-			{/* Completed */}
-			{/* ONgoing */}
-			{/* Upcoming */}
-			{/* each should be clickbale and redirect to the repspective position in page, scroll by id basically */}
 		</section>
 	);
 };
