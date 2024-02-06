2969a7ed731359242be40326162ec46b6bcd011e
kunalkeshan <kunalkeshan12@gmail.com>
2024-02-07 05:24:50 +0530

    chore: fetch queries for posts updated
    - get all w.r.t featured
    - get latest, limited to 5 recent

--- app/(main)/blogs/page.tsx
+++ app/(main)/blogs/page.tsx
@@ -7,11 +7,15 @@ const BlogsPage = async () => {
 	const posts = await sanityFetch<Array<SRCC_BlogPost>>({
 		query: queries.post.getAll,
 	});
+	const latestPosts = await sanityFetch<Array<SRCC_BlogPost>>({
+		query: queries.post.getLatest,
+	});
 	return (
 		<main className='w-full mt-[8.5rem] p-4 md:px-16 lg:max-w-7xl lg:mx-auto pb-8 md:pb-16 lg:pb-32'>
 			<h1 className='text-2xl md:text-3xl lg:text-4xl text-center text-app font-normal'>
 				Read our <span className='font-medium'>blogs</span>
 			</h1>
+			<section></section>
 			<Blogs posts={posts} />
 		</main>
 	);
