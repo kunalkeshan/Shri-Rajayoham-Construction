1af9963a3412cb1993cdf4764d641a7c4ceda4e4
kunalkeshan <kunalkeshan12@gmail.com>
2024-02-09 17:12:23 +0530

    chore: about page meta data updated

--- app/(main)/about/layout.tsx
+++ app/(main)/about/layout.tsx
@@ -7,14 +7,18 @@ const defaultMetadata = generateDefaultMetadata();
 export const metadata: Metadata = {
 	...defaultMetadata,
 	title: 'About | Shri Rajayoham Construction Company',
+	description:
+		'A realm where innovation intersects architecture, electrical prowess energizes spaces, civil engineering forms the bedrock, plumbing ensures comfort, environmental solutions weave sustainability, structural excellence provides strength, and interior design adds the finishing touch.',
 	openGraph: {
 		...defaultMetadata.openGraph,
 		title: 'About | Shri Rajayoham Construction Company',
+		description: `A realm where innovation intersects architecture, electrical prowess energizes spaces, civil engineering forms the bedrock, plumbing ensures comfort, environmental solutions weave sustainability, structural excellence provides strength, and interior design adds the finishing touch.`,
 		url: `${SRCC_WEBSITE_URL}/about`,
 	},
 	twitter: {
 		...defaultMetadata.twitter,
 		title: 'About | Shri Rajayoham Construction Company',
+		description: `A realm where innovation intersects architecture, electrical prowess energizes spaces, civil engineering forms the bedrock, plumbing ensures comfort, environmental solutions weave sustainability, structural excellence provides strength, and interior design adds the finishing touch.`,
 	},
 };
 
